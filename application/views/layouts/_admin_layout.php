89e180689b174c85db206734ebd8246efdba573a
Arnie Jothan Earl <ArnieDayaoen@gmail.com>
2016-04-06 10:43:01 +0800

    favicon

--- application/views/layouts/_admin_layout.php
+++ application/views/layouts/_admin_layout.php
@@ -17,7 +17,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" type="image/png" href="<?=site_url('favicon.png')?>" sizes="32x32" />
+    <link rel="shortcut icon" href="<?=site_url('favicon.png')?>">
+    <link rel="icon" href="<?=site_url('favicon.png')?>" type="image/x-icon">
+    
+    <!-- <link rel="icon" type="image/png" href="<?=site_url('favicon.png')?>" sizes="32x32" /> -->
 
     <title>
         <?php if ($title): ?>
