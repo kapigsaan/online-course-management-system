5dfd18dc6d5ea0cb0188ff1b71d80b33622f062f
Arnie Jothan Earl <ArnieDayaoen@gmail.com>
2016-04-20 15:40:22 +0800

    changes

--- application/views/layouts/_admin_layout.php
+++ application/views/layouts/_admin_layout.php
@@ -352,7 +352,7 @@
 
         $('#date_start').datepicker({ dateFormat: 'mm/dd/yy' });
         $('#date_end').datepicker({ dateFormat: 'mm/dd/yy' });
-        $('.datepick').datepicker({ dateFormat: 'mm/dd/yy' });
+        $('.datepick').datepicker({ dateFormat: 'Y-m-d' });
 
 
         $('a.confirm').on('click',function(e){
