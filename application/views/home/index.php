fb45db844162a266ac1f22672a25e04179d758ec
Arnie Jothan Earl <ArnieDayaoen@gmail.com>
2016-04-20 10:52:49 +0800

    asd

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -35,6 +35,7 @@
 <div id="home" >
 <div class="container">
 <div class="row">
+<?= $data['image']; ?>
 <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 ">
 <div id="carousel-slider" data-ride="carousel" class="carousel slide  animate-in" data-anim-type="fade-in-up">
 
