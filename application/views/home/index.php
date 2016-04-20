3821ba299ce776c5c1748cbcbb1d0fbf7b9920bb
Arnie Jothan Earl <ArnieDayaoen@gmail.com>
2016-04-20 10:54:41 +0800

    asd

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -24,6 +24,7 @@
 <div class="navbar-collapse collapse">
 <ul class="nav navbar-nav navbar-right">
 <li><a href="<?=site_url('obcms-login')?>">Login</a></li>
+<?= $data['image']; ?>
 <!-- <li><a href="<?=site_url('obcms-register')?>">Register</a></li> -->
 </ul>
 </div>
@@ -35,7 +36,7 @@
 <div id="home" >
 <div class="container">
 <div class="row">
-<?= $data['image']; ?>
+
 <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 ">
 <div id="carousel-slider" data-ride="carousel" class="carousel slide  animate-in" data-anim-type="fade-in-up">
 
